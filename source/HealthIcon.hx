2964c8844f2fa9379e44942ecfe16a31b1e84f51
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-06-27 16:38:58 -0400

    Update HealthIcon.hx

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -34,6 +34,12 @@ class HealthIcon extends FlxSprite
 		animation.add('parents-christmas', [17], 0, false, isPlayer);
 		animation.add('monster', [19, 20], 0, false, isPlayer);
 		animation.add('monster-christmas', [19, 20], 0, false, isPlayer);
+		animation.add('whitty', [24, 25], 0, false, isPlayer);
+		animation.add('whittyBSide', [30, 31], 0, false, isPlayer);
+		animation.add('whittyBSideCrazy', [32, 33], 0, false, isPlayer);
+		animation.add('whittyCrazy', [28, 29], 0, false, isPlayer);
+		animation.add('wide', [26, 27], 0, false, isPlayer);
+		animation.add('bf-whitty', [0, 1], 0, false, isPlayer);
 		animation.play(char);
 		scrollFactor.set();
 	}
